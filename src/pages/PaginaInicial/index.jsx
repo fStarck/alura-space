d1316379668088e39d58f65c678c7a6baaeba3d7
fstarckEpsoft <felipe.starck@epsoft.com.br>
2023-03-28 22:07:14 -0300

    adicionado fonte do projeto, variaveis padroes, adicionado a imagem do banner

--- src/pages/PaginaInicial/index.jsx
+++ src/pages/PaginaInicial/index.jsx
@@ -1,11 +1,21 @@
 import Cabecalho from "../../Componentes/Cabecalho";
 import Menu from "../../Componentes/Menu";
+import banner from './banner.png'
+import './PaginaInicial.css'
 
 export default function PaginaInicial () {
     return (
         <>
             <Cabecalho />
-            <Menu />
+            <main>
+                <section className="principal" >
+                    <Menu />
+                    <div className="principal__imagem">
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="a imagem da terra vista do espaço" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
